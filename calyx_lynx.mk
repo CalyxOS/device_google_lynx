4b1fd5673a0e87f4db6ca9956d764835fc9ad606
Michael Bestas <mkbestas@gmail.com>
2024-10-25 21:55:33 +0300

    Adapt to new build prop override scheme again
    Change-Id: Ifa5612ce2064af1f5d3a71dad29a5dbd4c1c4d39

--- calyx_lynx.mk
+++ calyx_lynx.mk
@@ -22,7 +22,7 @@ PRODUCT_MODEL := Pixel 7a
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc=lynx-user[[:space:]]15[[:space:]]AP3A.241005.015[[:space:]]12366759[[:space:]]release-keys \
+    BuildDesc="lynx-user 15 AP3A.241005.015 12366759 release-keys" \
     BuildFingerprint=google/lynx/lynx:15/AP3A.241005.015/12366759:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
