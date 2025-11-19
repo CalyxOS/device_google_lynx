LineageOS/android_device_google_lynx

lynx: Include additional wlan soong namespace
Change-Id: I1614960d2ca7a15f1646185986e2eae055e4b51d

device-lynx.mk
@@ -180,7 +180,9 @@ else
 endif
 
 # Wifi HAL
-PRODUCT_SOONG_NAMESPACES += hardware/qcom/wlan/wcn6740
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcomf/wlan \
+    hardware/qcom/wlan/wcn6740
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \

CalyxOS/device_google_lynx

device-lynx.mk
@@ -171,7 +171,9 @@ else
 endif
 
 # Wifi HAL
-PRODUCT_SOONG_NAMESPACES += hardware/qcom/wlan/wcn6740
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom/wlan \
+    hardware/qcom/wlan/wcn6740
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \